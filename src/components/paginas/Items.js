b485f29ad4dc8b2b96aeeff37f535c53dd98bbe0
programmertocode <children_park@hotmail.com>
2022-12-13 00:15:46 -0600

    Quinto Commit

---
+++ src/components/paginas/Items.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Items = () => {
+    return (
+        <div>
+            <h1>Esta es la página de items</h1>
+
+        </div>
+    )
+}
+
+export default Items; 
---
+++ src/components/paginas/Pokemon.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Pokemon = () => {
+    return (
+        <div>
+            <h1>Esta es la página de Pokemon</h1>
+
+        </div>
+    )
+}
+
+export default Pokemon; 
